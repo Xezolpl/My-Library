1e7da2db2be43f30e023ff51ab3d88dbbd1211eb
XezolPL <xezolpl@wp.pl>
2020-01-08 21:17:45 +0100

    Deleting dialogs

--- app/src/main/java/pl/xezolpl/mylibrary/utilities/DeletingHelper.java
+++ app/src/main/java/pl/xezolpl/mylibrary/utilities/DeletingHelper.java
@@ -1,11 +1,12 @@
 package pl.xezolpl.mylibrary.utilities;
 
+import android.graphics.Color;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import java.util.List;
+import java.io.Serializable;
 
 import pl.xezolpl.mylibrary.R;
 import pl.xezolpl.mylibrary.models.Book;
@@ -20,6 +21,8 @@ import pl.xezolpl.mylibrary.viewmodels.ChapterViewModel;
 import pl.xezolpl.mylibrary.viewmodels.NoteViewModel;
 import pl.xezolpl.mylibrary.viewmodels.QuoteCategoryViewModel;
 import pl.xezolpl.mylibrary.viewmodels.QuoteViewModel;
+import spencerstudios.com.ezdialoglib.EZDialog;
+import spencerstudios.com.ezdialoglib.Font;
 
 /**
  * This class provides deleting items from database with their children.
@@ -36,8 +39,16 @@ public class DeletingHelper {
     private AppCompatActivity activity = null;
     private Fragment fragment = null;
 
+    public static final int BOOK = 1;
+    public static final int CHAPTER = 2;
+    public static final int NOTE = 3;
+    public static final int QUOTECATEGORY = 4;
+    public static final int QUOTE = 5;
+
+
     /**
      * Setting up view models.
+     *
      * @param fragment for classes that inherit from Fragment.
      */
     public DeletingHelper(Fragment fragment) {
@@ -53,6 +64,7 @@ public class DeletingHelper {
 
     /**
      * Setting up view models.
+     *
      * @param activity for classes that inherit from AppCompactActivity.
      */
     public DeletingHelper(AppCompatActivity activity) {
@@ -68,43 +80,35 @@ public class DeletingHelper {
 
     /**
      * Deletes book with its categories, chapters, notes and optionally with quotes.
-     * @param book book which will be deleted.
+     *
+     * @param book       book which will be deleted.
      * @param withQuotes delete also that book's  quotes ?
      */
-    public void deleteBook(Book book, boolean withQuotes){
+    private void deleteBook(Book book, boolean withQuotes) {
         String bookId = book.getId();
 
-        if(withQuotes){
-            quoteViewModel.getQuotesByBook(bookId).observe(fragment == null ? activity : fragment, new Observer<List<Quote>>() {
-                @Override
-                public void onChanged(List<Quote> quotes) {
-                    for(Quote quote : quotes){
-                        //deletes every quote
-                        quoteViewModel.delete(quote);
-                    }
+        if (withQuotes) {
+            quoteViewModel.getQuotesByBook(bookId).observe(fragment == null ? activity : fragment, quotes -> {
+                for (Quote quote : quotes) {
+                    //deletes every quote
+                    quoteViewModel.delete(quote);
                 }
             });
         }
 
 
-        categoriesViewModel.getCategoriesByBook(bookId).observe(fragment == null ? activity : fragment, new Observer<List<CategoryWithBook>>() {
-            @Override
-            public void onChanged(List<CategoryWithBook> categoryWithBooks) {
-                for (CategoryWithBook category : categoryWithBooks){
-                    //deletes book's categories
-                    categoriesViewModel.delete(category);
-                }
+        categoriesViewModel.getCategoriesByBook(bookId).observe(fragment == null ? activity : fragment, categoryWithBooks -> {
+            for (CategoryWithBook category : categoryWithBooks) {
+                //deletes book's categories
+                categoriesViewModel.delete(category);
             }
         });
 
 
-        chapterViewModel.getChaptersByBook(bookId).observe(fragment == null ? activity : fragment, new Observer<List<Chapter>>() {
-            @Override
-            public void onChanged(List<Chapter> chapters) {
-                for (Chapter chapter : chapters){
-                    //deletes every chapter and its notes
-                    deleteChapter(chapter);
-                }
+        chapterViewModel.getChaptersByBook(bookId).observe(fragment == null ? activity : fragment, chapters -> {
+            for (Chapter chapter : chapters) {
+                //deletes every chapter and its notes
+                deleteChapter(chapter);
             }
         });
 
@@ -114,17 +118,15 @@ public class DeletingHelper {
 
     /**
      * Deletes chapter with its notes.
+     *
      * @param chapter chapter which will be deleted.
      */
-    public void deleteChapter(Chapter chapter){
+    private void deleteChapter(Chapter chapter) {
         String chapterId = chapter.getId();
 
-        noteViewModel.getNotesByParent(chapterId).observe(fragment == null ? activity : fragment, new Observer<List<Note>>() {
-            @Override
-            public void onChanged(List<Note> notes) {
-                for (Note note : notes){
-                    deleteNote(note);
-                }
+        noteViewModel.getNotesByParent(chapterId).observe(fragment == null ? activity : fragment, notes -> {
+            for (Note note : notes) {
+                deleteNote(note);
             }
         });
         chapterViewModel.delete(chapter);
@@ -133,17 +135,15 @@ public class DeletingHelper {
 
     /**
      * Deletes note with its children.
+     *
      * @param note note which will be deleted.
      */
-    public void deleteNote(Note note){
+    private void deleteNote(Note note) {
         String noteId = note.getId();
 
-        noteViewModel.getNotesByParent(noteId).observe(fragment == null ? activity : fragment, new Observer<List<Note>>() {
-            @Override
-            public void onChanged(List<Note> notes) {
-                for(Note note1 : notes){
-                    deleteNote(note1);
-                }
+        noteViewModel.getNotesByParent(noteId).observe(fragment == null ? activity : fragment, notes -> {
+            for (Note note1 : notes) {
+                deleteNote(note1);
             }
         });
         noteViewModel.delete(note);
@@ -153,24 +153,78 @@ public class DeletingHelper {
     /**
      * Deletes quoteCategory. If quoteCategory isn't "uncategorized" then deletes it.
      * Sets quoteCategory to "uncategorized" everywhere this category was used.
+     *
      * @param quoteCategory category which will be deleted
      */
-    public void deleteQuoteCategory(QuoteCategory quoteCategory){
+    private void deleteQuoteCategory(QuoteCategory quoteCategory) {
         String quoteCategoryId = quoteCategory.getId();
         final String uncategorized = fragment == null ? activity.getString(R.string.uncategorized) : fragment.getString(R.string.uncategorized);
 
-        quoteViewModel.getQuotesByCategory(quoteCategoryId).observe(fragment == null ? activity : fragment, new Observer<List<Quote>>() {
-            @Override
-            public void onChanged(List<Quote> quotes) {
-                for (Quote quote : quotes){
-                    //sets category to uncategorized in every quote where this category was used
-                    quote.setCategoryId(uncategorized);
-                }
+        quoteViewModel.getQuotesByCategory(quoteCategoryId).observe(fragment == null ? activity : fragment, quotes -> {
+            for (Quote quote : quotes) {
+                //sets category to uncategorized in every quote where this category was used
+                quote.setCategoryId(uncategorized);
             }
         });
 
-        if(!quoteCategoryId.equals(uncategorized)){
+        if (!quoteCategoryId.equals(uncategorized)) {
             quoteCategoryViewModel.delete(quoteCategory);
         }
+    }
+
+    public void showDeletingDialog(String title, String message, int type, Serializable itemToDelete) {
+
+         new EZDialog.Builder(fragment == null ? activity : fragment.getContext())
+                .setTitle(title)
+                .setMessage(message)
+
+                .setPositiveBtnText(fragment == null ? activity.getString(R.string.delete) : fragment.getString(R.string.delete))
+                .setNeutralBtnText(type == BOOK ? (fragment == null ?
+                        activity.getString(R.string.without_quotes) :
+                        fragment.getString(R.string.without_quotes)) : null)
+                .setNegativeBtnText(fragment == null ? activity.getString(R.string.cancel) : fragment.getString(R.string.cancel))
+
+                .OnPositiveClicked(() -> {
+                    switch (type) {
+                        case BOOK: {
+                            deleteBook((Book) itemToDelete, true);
+                            break;
+                        }
+                        case CHAPTER: {
+                            deleteChapter((Chapter) itemToDelete);
+                            break;
+                        }
+                        case NOTE: {
+                            deleteNote((Note) itemToDelete);
+                            break;
+                        }
+                        case QUOTECATEGORY: {
+                            deleteQuoteCategory((QuoteCategory) itemToDelete);
+                            break;
+                        }
+                        case QUOTE: {
+                            quoteViewModel.delete((Quote) itemToDelete);
+                            break;
+                        }
+                        default:
+                            break;
+
+                    }
+                })
+                .OnNeutralClicked(() ->
+                        deleteBook((Book) itemToDelete, false)
+                )
+                .OnNegativeClicked(() -> {
+                })
+
+                .setTitleDividerLineColor(Color.parseColor("#ed0909"))
+                .setTitleTextColor(Color.parseColor("#EE311B"))
+                .setButtonTextColor(Color.parseColor("#ed0909"))
+                .setMessageTextColor(Color.parseColor("#333333"))
+                .setFont(Font.COMFORTAA)
+
+                .setCancelableOnTouchOutside(true)
+                .build();
+
     }
 }
